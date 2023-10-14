035220ea25c529e2a8ba618b6f6eec214ffc55f1
“lianenguyen150git config --global user.email “lianenguyen150@gmail.com <lianenguyen150@gmail.com>
2023-10-13 22:51:03 -0400

    Add fonts

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="snap-mandatory snap-y h-screen w-screen overflow-scroll">
-      <div className="snap-start w-screen h-screen flex items-center justify-center">
-        <h1>Page 5</h1>
+      <div className="snap-start w-screen h-screen flex items-center justify-center font-andika">
+        <h1>Hello my name is liane</h1>
       </div>
-      <div className="snap-start w-screen h-screen flex bg-[#FFEEE2] items-center justify-center">
-        <h1>Page 2</h1>
+      <div className="snap-start w-screen h-screen flex bg-[#FFEEE2] items-center justify-center font">
+        <h1 className='font-delius'>Hello my name is Peyton</h1>
       </div>
       <div className="snap-start w-screen h-screen flex bg-[#F2EFC7] items-center justify-center">
-        <h1>Page 3</h1>
+        <h1 className='font-jua'>Liane and Peyton together forever</h1>
       </div>
     </div>
   );
